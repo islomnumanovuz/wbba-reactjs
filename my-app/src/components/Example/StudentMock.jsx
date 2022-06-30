139b98bc3c1acbfe4f0583654b59667196994cf7
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-06-30 14:03:40 +0500

    feat: Add onChangeName function in mock data

--- my-app/src/components/Example/StudentMock.jsx
+++ my-app/src/components/Example/StudentMock.jsx
@@ -12,8 +12,11 @@ export default class StudentMock extends Component {
     const onChangeId = ({ target: { value } }) => {
       let res = students.filter((student) => student.id === +value);
       this.setState({ students: res });
-
       !value.length && this.setState({ students: students });
+    };
+    const onChangeName = ({ target: { value } }) => {
+      let res = students.filter((student) => student.name.includes(value));
+      this.setState({ students: res });
     };
     const onDelete = (value) => {
       let res = this.state.students.filter(({ id }) => id !== value);
@@ -28,7 +31,7 @@ export default class StudentMock extends Component {
       <div className="container">
         <div className="input-group">
           <input onChange={onChangeId} type="number" placeholder="id" />
-          <input type="text" placeholder="name" />
+          <input onChange={onChangeName} type="text" placeholder="name" />
           <select name="" id="" onChange={onChangeStatus}>
             <option value="BS">BS</option>
             <option value="IT">IT</option>
--- my-app/src/mock/mock.js
+++ my-app/src/mock/mock.js
@@ -1,9 +1,9 @@
 export const students = [
-  { id: 1, name: "Islom Numanov", status: "BS" },
-  { id: 2, name: "Islom Numanov", status: "BS" },
+  { id: 1, name: "Sardor Mukhtorov", status: "BS" },
+  { id: 2, name: "Nozima Akhmadjonova", status: "BS" },
   { id: 3, name: "Islom Numanov", status: "IT" },
-  { id: 4, name: "Islom Numanov", status: "BS" },
-  { id: 5, name: "Islom Numanov", status: "CS" },
+  { id: 4, name: "Oyqiz Kimsanova", status: "BS" },
+  { id: 5, name: "Sardor Numanov", status: "CS" },
   { id: 6, name: "Islom Numanov", status: "BS" },
   { id: 7, name: "Islom Numanov", status: "IT" },
 ];
